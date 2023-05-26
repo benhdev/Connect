207d0a12bcb848148b7bdb9713bf71efaa55e74c
benhdev <me@benhirst.co.uk>
2023-05-26 23:40:18 +0100

    bug fixes

--- Framework/Debug.lua
+++ Framework/Debug.lua
@@ -8,6 +8,8 @@ type object = table<string>
 
 local module: module = {} :: module
 
+local RunService = game:GetService("RunService")
+
 function module.DebugEnabled (self, v)
     if typeof(v) == "boolean" or typeof(v) == "string" then
         self.DebugMode = v
@@ -28,6 +30,28 @@ function module.CallstackLevel (self)
     end
 
     return depth
+end
+
+function module.Counter (self: module, t: number?, key: string?)
+    task.spawn(function ()
+        while task.wait(t or 5) do
+            local counter = 0
+
+            if not key then
+                for _,connectionList in next, self.connections do
+                    for uuid,connection in next, connectionList do
+                        counter += 1
+                    end
+                end
+            else
+                for _,connection in next, self.connections[key] do
+                    counter += 1
+                end
+            end
+
+            print((if RunService:IsClient() then "Client: " else "Server: ") .. tostring(counter))
+        end
+    end)
 end
 
 return module
