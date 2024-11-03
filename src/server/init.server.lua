2293be252ff6f3af57ed45fb1911d11e0a81d75a
benhdev <30080152+benhdev@users.noreply.github.com>
2024-11-03 16:06:18 +0000

    test

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,36 +1,13 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
 local Connect = require(ReplicatedStorage:WaitForChild("ConnectFramework"))
-Connect:UseDataStore(7)
 
 -- Create the session
 local Session = Connect:session()
+local Event = Connect:event()
 
--- Register a global onUpdate handler
-Session:onUpdate(function (self, key, value)
-    -- print(`{key}: {value}`)
-end)
-
--- Register the PlayerAdded Connection
-local connection = Connect:create("PlayerAdded", function (self, Player)
-    -- Create the leaderboard
-    local Leaderstats = Instance.new("StringValue")
-    Leaderstats.Name = "leaderstats"
-
-    -- Create the points value for the leaderboard
-    local Points = Instance.new("IntValue")
-    Points.Name = "Points"
-
-    -- Create a Key for the Player's points
-    local key = Session:key(Player.UserId, "Points")
-
-    -- Register a Key specific onUpdate handler
-    Session:onUpdate(key, function (self, value)
-        Points.Value = value
-    end)
-
+Event:listen("fetch", function (Player, key)
     -- Fetch the player's saved data for this key
-    local DataStoreRequest = Connect:fetch(key, function (self, response)
+    return Connect:fetch(key, function (self, response)
         Connect.tick(function (i)
             -- Increase the points each second
             Session:update(key, (Session:find(key) or response or 0) + 1)
@@ -39,22 +16,38 @@ local connection = Connect:create("PlayerAdded", function (self, Player)
             return not (Player and Player.Parent)
         end)
     end)
+end)
 
+Event:listen("createLeaderboard", function (key)
+    -- Create the leaderboard
+    local Leaderstats = Instance.new("StringValue")
+    Leaderstats.Name = "leaderstats"
+
+    -- Create the points value for the leaderboard
+    local Points = Instance.new("IntValue")
+    Points.Name = "Points"
+
+    -- Register a Key specific onUpdate handler
+    Session:onUpdate(key, function (self, value)
+        Points.Value = value
+    end)
+
+    return Leaderstats, Points
+end)
+
+-- Register the PlayerAdded Connection
+local connection = Connect:create("PlayerAdded", function (self, Player)
+    -- Create a Key for the Player's points
+    local key = Session:key(Player.UserId, "Points")
+
+    local Leaderstats, Points = Event:fire("createLeaderboard", key)
+
+    local DataStoreRequest = Event:fire("fetch", Player, key)
     -- Wait for the DataStoreRequest to finish
     DataStoreRequest:sync()
 
     Points.Parent = Leaderstats
     Leaderstats.Parent = Player
-end)
-
-connection:onDisconnect(function (self)
-
-end)
-
-Connect.tick(5, function ()
-
-end, function () 
-    return not connection.Connected
 end)
 
 -- Register the PlayerRemoving connection
@@ -68,3 +61,5 @@ Connect:create("PlayerRemoving", function (self, Player)
         Session:remove(key)
     end)
 end)
+
+-- Connect:DebugEnabled("internal")
